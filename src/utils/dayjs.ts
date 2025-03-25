dbb0f6abfc5d0d27add591965a98f9403318fe39
somsak.mun <goszila007@gmail.com>
2025-03-25 09:52:10 +0700

    :hammer: fix bug get friday and saturday date

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,16 +1,28 @@
 import dayjs from "dayjs";
 
 // get all friday and saturday dates in the next 3 months function
-export function getDates() {
+export function getDates(numDays: number[]) {
   const dates = [];
-  const today = dayjs();
-  for (let i = 0; i < 3; i++) {
-    const date = today.add(i, "month");
-    const friday = date.day(5).format("YYYY-MM-DD");
-    const saturday = date.day(6).format("YYYY-MM-DD");
-    dates.push(friday, saturday);
+  let currentMonth = dayjs().month() + 1;
+  for (let i = currentMonth; currentMonth < i + 3; currentMonth++) {
+    dates.push(...getFridaysInMonth(2025, currentMonth));
   }
   return dates;
+}
+
+function getFridaysInMonth(year: number, month: number) {
+  const fridays = [];
+  const daysInMonth = dayjs(`${year}-${month}-01`).daysInMonth();
+
+  for (let day = 1; day <= daysInMonth; day++) {
+    const date = dayjs(`${year}-${month}-${day}`);
+    const currentDate = dayjs();
+    if ([5, 6].includes(date.day()) && date.isAfter(currentDate)) { // 5 represents Friday
+      fridays.push(date.format('YYYY-MM-DD'));
+    }
+  }
+
+  return fridays;
 }
 
 // convert date to string to DD/MM/YYYY function
